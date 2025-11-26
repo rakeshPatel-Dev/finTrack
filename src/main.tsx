610744d56be19105b3b7efe8f76269ed03d24e6f
Rakesh Patel <rk5080976@gmail.com>
2025-11-26 06:17:50 +0545

    Some changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-  
+  <StrictMode/>
   <App />
+<StrictMode/>
   </BrowserRouter>
 )
